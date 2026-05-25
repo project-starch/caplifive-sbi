7f1b9e8dcde44cc1cbb8bcff7043ec9c0f04d9e0
Jason Yu <yuzhijingcheng1996@hotmail.com>
2026-05-25 05:13:03 +0000

    Fixed cap type encodings and other minor issues

--- sbi_capstone.c
+++ sbi_capstone.c
@@ -386,7 +386,7 @@ static unsigned shared_region_annotated(unsigned dom_id, unsigned region_id, uns
     }
     else if (annotation_rev == CAPSTONE_ANNOTATION_REV_SHARED) {
         // capability type: non-linear; post-return revoke: no
-        if (cap_type(r) == 1) {
+        if (cap_type(r) == CAP_TYPE_LINEAR) {
             r = __delin(r);
 
             if (region_cpmp[region_id] != -1) {
@@ -400,7 +400,7 @@ static unsigned shared_region_annotated(unsigned dom_id, unsigned region_id, uns
     else if (annotation_rev == CAPSTONE_ANNOTATION_REV_TRANSFERRED) {
         // capability type: linear; post-return revoke: no
         // TODO: regions[region_id] should be added to a free list
-        if (cap_type(r) != 1) {
+        if (cap_type(r) != CAP_TYPE_LINEAR) {
             //C_PRINT(0xdeadbeef);
             while(1);
         }
@@ -722,13 +722,17 @@ static void swap_cpmp(unsigned badaddr) {
 }
 
 unsigned handle_exception(unsigned cause) {
-    unsigned badaddr;
+    unsigned badaddr, epc;
     unsigned time_val;
     switch(cause) {
         case CAUSE_ILLEGAL_INSTRUCTION:
+            // __asm__ ("1: j 1b");
             C_READ_CSR(mtval, badaddr);
             if (((badaddr & 0xFFF0707F) == CSR_TIME)) {
                 time_val = *mtime;
+                // __asm__ ("csrr %0, mepc" : "=r"(epc));
+                // epc += 4;
+                // __asm__ ("csrw mepc, %0" :: "r"(epc));
                 break;
             }
             else {
